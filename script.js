9b9c0036aae30118aff3662f3176bf1f783986dc
Vitalii Dushenko <114583341+VD-Coderr@users.noreply.github.com>
2023-01-26 13:16:31 +0100

    Game works in a browser console. Final winner is only a message for now.

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // getting random Computer choice
-
 function getComputerChoice() {
   const randomNumber = Math.floor(Math.random() * 3);
   
@@ -20,10 +19,15 @@ function getComputerChoice() {
 let playerScore = 0;
 let computerScore = 0;
 
-const playerSelection = prompt('Make you move...');
-const computerSelection = getComputerChoice();
+let playerSelection
+let computerSelection
 
-// function to play a round and evaluate who is a winner.
+// updateting choices of user and computer before next round
+function getChoices() {
+  playerSelection = prompt('Make you move...');
+  computerSelection = getComputerChoice();
+}
+// function to play a round, evaluate who is a winner, display a message and add score
 function playRound(playerSelection, computerSelection) {
 
   let winningStatement = `You won! ${playerSelection} beats ${computerSelection}`;
@@ -37,11 +41,37 @@ function playRound(playerSelection, computerSelection) {
   } else if ((playerSelection === 'rock' && computerSelection === 'paper') || 
             (playerSelection === 'paper' && computerSelection === 'scissors') || 
             (playerSelection === 'scissors' && computerSelection === 'rock')) {
-    computerScore++;
-    return loosingStatement;
+      computerScore++;
+      return loosingStatement;
   } else if ((playerSelection === computerSelection) && (playerSelection != undefined)) {
-    return "Draw!"
+      return "Draw!"
   } else {
-    return 'Make your choice.'
+      return 'Make your choice.'
   }
+}
+
+// checks if anybody won the game (5 scores)
+function isGameOver() {
+  return playerScore === 5 || computerScore === 5;
+}
+
+// displays message of score after each round or final message after someone won
+function updateScore() {
+  if (isGameOver()) {
+  return `Game Over: You: ${playerScore}; Computer: ${computerScore}`;
+} else {
+  return `Score: You: ${playerScore}; Computer: ${computerScore}`;
+}
+}
+
+// keeps game in correct order: choose your weapon, play a round nad show the result
+function game() {
+  getChoices();
+  console.log(playRound(playerSelection, computerSelection));
+  console.log(updateScore());
+}
+
+// for loop to play 5 games
+for(let i = 1; i < 6; i++) {
+  console.log(game());
 }
